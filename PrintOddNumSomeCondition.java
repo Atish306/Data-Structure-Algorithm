2321b9bef4b4deea55a120a640fd4d50affca02c
Atish Chauhan <131800489+Atish306@users.noreply.github.com>
2025-02-15 12:17:10 +0530

    Create PrintOddNumSomeCondition.java

---
+++ PrintOddNumSomeCondition.java
@@ -0,0 +1,22 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int count = 0;
+		int num = 465456;
+		while(num /2 != 0){
+		    num = num /2;
+		    
+		    if (num % 2 == 0){
+		        continue;
+		        } 
+		         if ( num % 2 != 0 && count  < 5) {
+		             
+		            System.out.println(num+" ");
+		        }
+		        else{
+		            break;
+		        }
+		       count++; 
+		}
+	}
+}
