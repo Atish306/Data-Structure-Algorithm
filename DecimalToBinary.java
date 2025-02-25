66916eb27f885ad332724cdddac85476b67d4f46
Atish Chauhan <131800489+Atish306@users.noreply.github.com>
2025-02-25 08:29:50 +0530

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,4 +1,20 @@
 
 
+public class DecimalToAnyBase {
+    public static void main(String[] args) {
+        decimalToAnybase(357,10);
+    }
+    public static void decimalToAnybase(int num , int base){
+        int ans = 0;
+        int power = 0;
+        while(num > 0){
+            int digit = num%2;
+            num /=2;
+            ans = (int)(digit*Math.pow(base,power) + ans);
+            power++;
+        }
+        System.out.println("Binary of the number "+ans);
+    }
+}
 
 
